b45069188940d86963e90f7d19e7d63c1f340e21
fimmy <fimmy@live.cn>
2025-07-15 12:33:05 +0800

    更新配置

--- both_double_quanping.py
+++ both_double_quanping.py
@@ -5,24 +5,14 @@ import datetime
 
 rime_path = os.path.join(os.environ.get("AppData"), "Rime")
 wanxiang_schema_path = os.path.join(rime_path, "wanxiang.schema.yaml")
-wanxiang_double_schema_path = os.path.join(rime_path, "wanxiang_double.schema.yaml")
-bak_dir_path = os.path.join(rime_path, "bak")
+wanxiang_pinyin_schema_path = os.path.join(rime_path, "wanxiang_pinyin.schema.yaml")
 if os.path.exists(wanxiang_schema_path):
-    # if os.path.exists(wanxiang_double_schema_path):
-    #     p = Path(wanxiang_double_schema_path)
-    #     bak_file_path = os.path.join(
-    #         bak_dir_path,
-    #         f'{p.stem}_{datetime.datetime.now().strftime("%Y%m%d%H%M%S")}{p.suffix}',
-    #     )
-    #     shutil.copy(wanxiang_double_schema_path, bak_file_path)
-    shutil.copy(wanxiang_schema_path, wanxiang_double_schema_path)
-
-    with open(wanxiang_double_schema_path, "r", encoding="utf-8") as file:
+    shutil.copy(wanxiang_schema_path, wanxiang_pinyin_schema_path)
+    with open(wanxiang_pinyin_schema_path, "r", encoding="utf-8") as file:
         content = file.read()
 
     # 2. 在内存中进行替换
-    new_content = content.replace("schema_id: wanxiang", "schema_id: wanxiang_double")
-
+    new_content = content.replace("schema_id: wanxiang", "schema_id: wanxiang_pinyin")
     # 如果你确定要直接覆盖原文件，可以这样做：
-    with open(wanxiang_double_schema_path, "w", encoding="utf-8") as file:
+    with open(wanxiang_pinyin_schema_path, "w", encoding="utf-8") as file:
         file.write(new_content)
